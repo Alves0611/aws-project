edc4e8f718f2bcdf6ab1945324071f0b1ea79bf3
Alves0611 <g.alves0611@gmail.com>
2024-11-22 16:19:44 -0300

    refactor: update subnet resource reference from 'public' to 'publics'

--- vpc.nat-gateway.tf
+++ vpc.nat-gateway.tf
@@ -1,6 +1,6 @@
 resource "aws_nat_gateway" "this" {
   allocation_id = aws_eip.this.id
-  subnet_id     = aws_subnet.public[0].id
+  subnet_id     = aws_subnet.publics[0].id
 
   tags = merge(var.tags, { Name = var.vpc.nat_gateway_name })
 
