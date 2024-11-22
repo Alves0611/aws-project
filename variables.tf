3f35ac11993cfae9b484c83468a6693baab2812b
Alves0611 <g.alves0611@gmail.com>
2024-11-22 13:30:29 -0300

    feat: add public subnets configuration with CIDR blocks and availability zones

--- variables.tf
+++ variables.tf
@@ -9,14 +9,45 @@ variable "tags" {
 
 variable "vpc" {
   type = object({
-    name                  = string
-    cidr_block            = string
+    name       = string
+    cidr_block = string
+
     internet_gateway_name = string
+
+    public_subnets = list(object({
+      name                    = string
+      map_public_ip_on_launch = bool
+      availability_zone       = string
+      cidr_block              = string
+    }))
   })
 
   default = {
-    name                  = "studying-vpc"
-    cidr_block            = "10.0.0.0/24"
+    name       = "studying-vpc"
+    cidr_block = "10.0.0.0/24"
+
     internet_gateway_name = "studying-igw"
+
+    public_subnets = [
+      {
+        name                    = "public-us-east-1a"
+        map_public_ip_on_launch = true
+        availability_zone       = "us-east-1a"
+        cidr_block              = "10.0.0.0/26"
+      },
+      {
+        name                    = "public-us-east-1b"
+        map_public_ip_on_launch = true
+        availability_zone       = "us-east-1b"
+        cidr_block              = "10.0.0.64/26"
+      },
+      {
+        name                    = "public-us-east-1c"
+        map_public_ip_on_launch = true
+        availability_zone       = "us-east-1c"
+        cidr_block              = "10.0.0.128/26"
+      }
+
+    ]
   }
 }
