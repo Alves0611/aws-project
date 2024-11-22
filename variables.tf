25128ef8c2091063686ab6368f8f5aad9f4576e7
Alves0611 <g.alves0611@gmail.com>
2024-11-22 16:05:51 -0300

    fix: correct variable definitions for VPC and EKS configurations

--- variables.tf
+++ variables.tf
@@ -6,7 +6,6 @@ variable "tags" {
   }
 }
 
-
 variable "vpc" {
   type = object({
     name       = string
@@ -19,6 +18,13 @@ variable "vpc" {
     eip                      = string
 
     public_subnets = list(object({
+      name                    = string
+      map_public_ip_on_launch = bool
+      availability_zone       = string
+      cidr_block              = string
+    }))
+
+    private_subnets = list(object({
       name                    = string
       map_public_ip_on_launch = bool
       availability_zone       = string
@@ -50,11 +56,13 @@ variable "vpc" {
         cidr_block              = "10.0.0.64/26"
       }
     ]
-    private_subnets = [{
-      name                    = "private-us-east-1a"
-      map_public_ip_on_launch = false
-      availability_zone       = "us-east-1a"
-      cidr_block              = "10.0.0.128/26"
+
+    private_subnets = [
+      {
+        name                    = "private-us-east-1a"
+        map_public_ip_on_launch = false
+        availability_zone       = "us-east-1a"
+        cidr_block              = "10.0.0.128/26"
       },
       {
         name                    = "private-us-east-1b"
@@ -63,44 +71,5 @@ variable "vpc" {
         cidr_block              = "10.0.0.192/26"
       }
     ]
-  }
-}
-
-variable "eks_cluster" {
-  type = object({
-    name                              = string
-    role_name                         = string
-    enabled_cluster_log_types         = list(string)
-    access_config_authentication_mode = string
-    node_group = object({
-      name                        = string
-      role_name                   = string
-      instance_types              = list(string)
-      scaling_config_max_size     = number
-      scaling_config_min_size     = number
-      scaling_config_desired_size = number
-    })
-  })
-
-  default = {
-    name     = "studying-eks-cluster"
-    role_name = "StudyingEKSClusterRole"
-    enabled_cluster_log_types = [
-      "api",
-      "audit",
-      "authenticator",
-      "controllerManager",
-      "scheduler"
-    ]
-    acces_config_authentication_mode = "API_AND_CONFIG_MAP"
-    node_group = {
-      name                        = "studying-eks-cluster-node-group"
-      role_name                   = "StudyingEKSClusterNodeGroup"
-      instance_types              = ["t3.medium"]
-      scaling_config_max_size     = 2
-      scaling_config_min_size     = 2
-      scaling_config_desired_size = 2
-
-    }
   }
 }
