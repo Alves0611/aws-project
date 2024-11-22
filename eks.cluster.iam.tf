fcebae2174a392ba9f8ef357b8681d4f824c1291
Alves0611 <g.alves0611@gmail.com>
2024-11-22 15:35:21 -0300

    refactor: parameterize IAM role and policy configuration for EKS cluster

--- eks.cluster.iam.tf
+++ eks.cluster.iam.tf
@@ -12,7 +12,7 @@ data "aws_iam_policy_document" "eks_cluster_role" {
 }
 
 resource "aws_iam_role" "eks_cluster_role" {
-  name               = "StudyingEKSClusterRole"
+  name               = var.eks_cluster.role_arn
   assume_role_policy = data.aws_iam_policy_document.eks_cluster_role.json
 }
 
