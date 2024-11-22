1a741c1d5f013a4cce2fb7ad51dae17071ddc7cf
Alves0611 <g.alves0611@gmail.com>
2024-11-22 13:44:09 -0300

    feat: add AWS public route table with internet gateway route

---
+++ vpc.public-route-table.tf
@@ -0,0 +1,10 @@
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.this.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.this.id
+  }
+
+  tags = merge(var.tags, { Name = var.vpc.public_route_table_name })
+}
