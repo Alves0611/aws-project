cf5aa5aac76a4ed8bf800be975fb971aca4446e5
Alves0611 <g.alves0611@gmail.com>
2024-11-22 15:22:11 -0300

    feat: add AWS EKS cluster resource with configuration

---
+++ eks.cluster.tf
@@ -0,0 +1,24 @@
+resource "aws_eks_cluster" "this" {
+  name = "example"
+
+  role_arn = aws_iam_role.eks_cluster_role.arn
+  enabled_cluster_log_types = [
+    "api",
+    "audit",
+    "authenticator",
+    "controllerManager",
+    "scheduler"
+  ]
+
+  vpc_config {
+    subnet_ids = aws_subnet.privates[*].id
+  }
+
+  access_config {
+    authentication_mode = "API_AND_CONFIG_MAP"
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.eks_cluster_role_AmazonEKSClusterPolicy
+  ]
+}
