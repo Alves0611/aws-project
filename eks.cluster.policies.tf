543c5833102cf0ed82d28aed3176304c583dc194
Alves0611 <g.alves0611@gmail.com>
2024-11-22 15:37:53 -0300

    feat: add EKS access policy association with dynamic account ID

---
+++ eks.cluster.policies.tf
@@ -0,0 +1,11 @@
+data "aws_caller_identity" "current" {}
+
+resource "aws_eks_access_policy_association" "this" {
+  cluster_name  = aws_eks_cluster.this.name
+  policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+  principal_arn = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:user/studying"
+
+  access_scope {
+    type = "cluster"
+  }
+}
